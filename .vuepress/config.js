02e10bf5ee7152da9ea44548ccd7eca57bdeeb37
netlesh <netlesh@mail.ru>
2019-12-06 16:15:56 +0300

    vuepress-plugin-contact-us eklendi

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -14,10 +14,23 @@ module.exports = {
       },
       { text: 'SSS ', link: '/sss' },
       { text: 'EkolFX', link: '/EkolFX' },
+      { text: 'İletişim', link: '/Iletisim' },
     ],
     sidebar: [
       ['http://ahmetmusakosali.forex21pro3.c2strack.com', 'ProFX Satın Al'],
       ['https://www.ekolfx5.com/hesap-ac?ref=1959&utm_source=refout&utm_term=1959', 'EkolFX\'de Hesap Aç']
     ]
-  }
+  },
+  plugins:[
+    [
+        'contact-us', // Or 'vuepress-plugin-contact-us'
+        {
+            /**
+             * Mandatory: the email address which the contact data will be sent
+             */
+            email: 'iletisim@ahmetmusakosali.com.tr',
+           
+        }
+    ]
+]
 }
---
+++ Iletisim.md
@@ -0,0 +1,3 @@
+# Bize yazın
+
+
